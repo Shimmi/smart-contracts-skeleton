db234e401aef8ea9a35ce76f0d3c618669d07587
Jiří Šimeček <jirka@simecek.org>
2018-01-25 11:07:00 +0100

    Remove development network from configuration.
    So the network does not have to be specified while running tests.

--- truffle-config.js
+++ truffle-config.js
@@ -10,11 +10,6 @@ module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
     networks: {
-        development: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: "*"
-        },
         coverage: {
             host: "localhost",
             port: 8555,
